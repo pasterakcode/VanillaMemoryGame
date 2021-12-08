c2629f4d82939f134f8f0104969410dc5ffecfb9
pasterakcode <pasterakcode@gmail.com>
2021-12-09 00:08:44 +0100

    add sectionSelect, sectionAction, sectionGame

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
 const cardsQty = 9;
 
 let gameTask = [];
-let gameSolution = [];
-const allGameLevels = [0, 1, 2, 3, 4, 5];
-const gameTaskCards = Array.from(document.querySelectorAll('.gameTask .card'));
-const gameSolutionCards = Array.from(
-	document.querySelectorAll('.gameSolution .card')
-);
+let gameSolve = [];
+let currentLevel = 0;
+const allGameLevels = [];
+const inputSelectLevels = document.querySelector('input.inputSelectLevels');
+const paragraphInputValue = document.querySelector('p.inputValue');
+const gameTaskCards = document.querySelectorAll('.gameTask .card');
+const gameSolveCards = document.querySelectorAll('.gameSolve .card');
+const paragraphSelectedCard = document.querySelector('p.selectedCard');
+const btnStart = document.querySelector('.btnStart');
+const btnReset = document.querySelector('.btnReset');
+const levelCounterTask = document.querySelector('.gameTask .levelCounter');
+const levelCounterSolve = document.querySelector('.gameSolve .levelCounter');
 
+const addEventListeners = () => {
+	inputSelectLevels.addEventListener('change', () => {
+		paragraphInputValue.textContent = inputSelectLevels.value;
+	});
+	btnStart.addEventListener('click', startGame);
+	[...gameSolveCards].forEach(el =>
+		el.addEventListener('click', saveUserChoisedCard)
+	);
+};
+
+const startGame = () => {
+	setDrownCards();
+	setLevelCounter();
+	paintCurrentCardAndLevel(currentLevel);
+	currentLevel++;
+};
+const resetGame = () => {
+	gameTask = [];
+	gameSolve = [];
+	currentLevel = 0;
+};
+// prepare to game
 const drawOneCard = () => {
 	const getRandomInt = max => {
 		return Math.floor(Math.random() * max);
@@ -15,14 +43,54 @@ const drawOneCard = () => {
 	const drownCard = gameTaskCards[getRandomInt(cardsQty)];
 	return drownCard;
 };
-const saveDrownCard = () => {
-	gameTask.push(drawOneCard());
+const setDrownCards = () => {
+	for (let i = 1; i <= inputSelectLevels.value; i++) {
+		const newSelectedCard = drawOneCard();
+		gameTask.push(newSelectedCard);
+	}
 };
-const paintDrownCard = () => {
-    gameTask[0].style.backgroundColor = "red";
-}
-console.log(saveDrownCard());
-console.log(saveDrownCard());
-console.log(saveDrownCard());
-paintDrownCard();
-console.log(gameTask);
+const setLevelCounter = () => {
+	for (let i = 1; i <= inputSelectLevels.value; i++) {
+		allGameLevels.push(`<div class="level" id="level.${i}">${i}</div>`);
+	}
+	levelCounterTask.innerHTML = allGameLevels.join('');
+	levelCounterSolve.innerHTML = allGameLevels.join('');
+};
+
+// game operation
+const paintCurrentCardAndLevel = level => {
+	const currentCardTask = gameTask[level];
+	const currentLevelTask = levelCounterTask.children[level];
+	currentCardTask.style.backgroundColor = 'green';
+	currentLevelTask.style.backgroundColor = 'green';
+	setTimeout(() => {
+		currentCardTask.style.backgroundColor = 'transparent';
+	}, 500);
+};
+const paintAFewLevels = level => {
+	for (let i = 0; i <= level; i++) {
+		setTimeout(() => {
+			paintCurrentCardAndLevel(i);
+		}, 1000);
+	}
+};
+const saveUserChoisedCard = e => {
+	gameSolve.push(e.target);
+	checkSelectedCard();
+};
+const checkSelectedCard = () => {
+	const i = currentLevel - 1;
+	const shouldBeChoised = gameTask[i].id;
+	const choised = gameSolve[i].id;
+	if (shouldBeChoised === choised) {
+		console.log('good choise');
+		nextLevel();
+	} else {
+		console.log('bad choise');
+	}
+};
+const nextLevel = () => {
+	paintAFewLevels(currentLevel);
+};
+
+window.addEventListener('DOMContentLoaded', addEventListeners);
